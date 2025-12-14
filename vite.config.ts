057db84618fb343848d7f2fafcbc318fd6535bc3
maimu-s <maimu-s@example.com>
2025-12-15 02:56:45 +0900

    security update

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       compress: {
         drop_console: true,
         drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug'],
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.error'],
       },
       mangle: {
         toplevel: true,
